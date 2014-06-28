4a99527dd7a607456106b47c14c438c0e36f555f
Ignacio Freiberg <hookdump@gmail.com>
2014-06-28 17:39:58 -0300

    init sockets!

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// server.js (Express 4.0)
 var express        = require('express');
 var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
@@ -9,11 +8,18 @@ var app            = express();
 var zmq = require('zmq');
 var subscriber = zmq.socket('sub');
 var fs = require('fs');
+var mySocket = null;
 
+// inicializar sockets
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function (socket) {
+  mySocket = socket;
+});
+
+// inicializar el server
 app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 		// loguear todo!
 app.use(bodyParser()); 			// agarrar informacion en POST
 app.use(methodOverride()); 	// capturar DEL y PUT
-
 app.listen(8080);
 console.log('Escuchando puerto 8080');
