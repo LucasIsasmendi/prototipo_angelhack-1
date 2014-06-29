8bc65d8e24e7b54315b3aef40e528bd8a6ea11ae
Nicolas Kovalsky <kovalsky.nicolas@gmail.com>
2014-06-28 21:53:04 -0300

    test with audio

--- public/www/js/app.js
+++ public/www/js/app.js
@@ -1,17 +1,31 @@
 
 console.log("hohohoads");
 function login(){
-  FB.login(function(response) {
-    console.log("Logged In");
-  }, {scope: 'publish_actions,user_status,friends_status,read_stream'});
+	FB.login(function(response) {
+		console.log("Logged In");
+	}, {scope: 'publish_actions,user_status,friends_status,read_stream'});
 }
 
 function getFeed(){
 
-FB.api('/me/home?locale=es_LA', function(response) {
-  console.log(response);
-});
+	FB.api('/me/home?locale=es_LA', function(response) {
+		console.log(response);
+	});
 
+}
+
+
+function hablar(){
+	var video = document.getElementById("Video1");
+	//var button = document.getElementById("play");
+	video.play();
+	//if (video.paused) {
+	//	video.play();
+		//button.textContent = "||";
+	//} else {
+	//	video.pause();
+		//button.textContent = ">";
+	//}
 }
 
 
